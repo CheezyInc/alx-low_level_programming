aefad29b95f4daaa10e2042adeab080785f7414d
Chizzy <chizzyegbulefu@yahoo.com>
2022-06-13 03:11:23 -0700

    first

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib>
+#include <stdio>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low = 'a'
+	
+	while (low <= 'z')
+	{
+		putchar(low);
+		low++;
+	}
+
+	char low = 'A'
+	while (low <= 'Z')
+	{
+		putchar(low);
+		low++;
+	}
+
+	putchar('\n');
+return (0);
+}

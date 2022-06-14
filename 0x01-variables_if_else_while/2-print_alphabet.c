710a6d688e8e9018cd78050308616bf9c4020b64
Chizzy <chizzyegbulefu@yahoo.com>
2022-06-14 02:34:19 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,16 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-char low;
+	char c = 'a';
 
-for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
 	putchar('\n');
-
-return (0);
+	return (0);
 }
